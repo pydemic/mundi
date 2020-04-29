c6a2e283728c17f3c0b0e266c44f80d6ca9c2f30
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2020-04-29 16:31:06 -0300

    Tweak -m usage

--- mundi/__main__.py
+++ mundi/__main__.py
@@ -1,17 +1,15 @@
 import click
-from mundi import regions
+import mundi
 
 
 @click.command("mundi")
-@click.option("--country_code", "-c", help="Country code")
+@click.option("--country", "-c", help="Country code")
 @click.option("--type", "-t", default="country", help="Region type")
-def cli(**kwargs):
-    if "type" in kwargs:
-        kwargs["type"] = kwargs["type"].lower()
-        if kwargs["type"] == "all":
-            del kwargs["type"]
-
-    df = regions(**kwargs)
+def cli(country=None, **kwargs):
+    if country:
+        df = mundi.region(country)
+    else:
+        df = mundi.regions(**kwargs)
     print(df)
 
 
